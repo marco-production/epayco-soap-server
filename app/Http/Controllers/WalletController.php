0cc02da6d4c28c2566849ee847fcd78eb89cb70d
Marco De la cruz <marco033F@hotmail.com>
2022-12-20 01:03:59 -0400

    feature/Add migrations

---
+++ app/Http/Controllers/WalletController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class WalletController extends Controller
+{
+    public function RegistroCliente()
+    {
+        
+    }
+}
